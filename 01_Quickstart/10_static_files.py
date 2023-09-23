da42e3f9499f9af04352e8c4853ec52c3293a510
anil <Anil-Gehlot>
2023-09-23 18:31:17 +0530

    what is static files and their usage

---
+++ 01_Quickstart/10_static_files.py
@@ -0,0 +1,18 @@
+
+'''
+Static Files are contained by the static folder.
+
+This folder will contain your static files such as CSS, JavaScript, and images, video etc.
+
+ To generate URLs for static files in your templates or code,
+	SYNTAX :url_for('static', filename='style.css')
+
+
+		flask_app_name/
+		|
+		├── static/
+		│   └── style.css
+		└── app.py
+
+
+'''
